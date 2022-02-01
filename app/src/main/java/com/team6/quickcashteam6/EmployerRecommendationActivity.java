4fa6e5af36865e167cf489bbcf0bb5ecc463211a
Alkhalil Alsuleimani <alkhalilalsuleimani@Alkhalils-MacBook-Pro.local>
2022-02-01 14:32:43 -0400

    Added test for get name

---
+++ app/src/main/java/com/team6/quickcashteam6/EmployerRecommendationActivity.java
@@ -0,0 +1,8 @@
+package com.team6.quickcashteam6;
+
+public class EmployerRecommendationActivity {
+
+    public static void recommendation(User user) {
+
+    }
+}
---
+++ app/src/test/java/com/team6/quickcashteam6/EmployerRecommendationActivityTest.java
@@ -0,0 +1,15 @@
+package com.team6.quickcashteam6;
+
+import junit.framework.TestCase;
+
+public class EmployerRecommendationActivityTest extends TestCase {
+
+    public void testRecommendation() {
+        User user;
+    }
+    
+    public void getName()  {
+        User user;
+        assertNotNull("Wong name retrieve", user.getName());
+    }
+}
