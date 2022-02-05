a3e8faed9fcdd196a56aa8bf94eaad04c3a3291b
Ayat Kamona <ay245494@dal.ca>
2022-02-05 19:57:22 -0400

    Base java files created

---
+++ app/src/test/java/com/team6/quickcashteam6/US6_JUnitTests.java
@@ -0,0 +1,13 @@
+package com.team6.quickcashteam6;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class US6_JUnitTests {
+}
