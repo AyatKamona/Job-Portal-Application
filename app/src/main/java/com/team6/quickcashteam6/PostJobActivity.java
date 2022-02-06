fff7a952f6a443d3728971d6e4f6cd9f3fc3b34f
Elyas Al Belushi <el992401@dal.ca>
2022-02-05 21:54:00 -0400

    Made checkIfMovedToPostJobPage() Espresso test pass.

--- app/src/main/java/com/team6/quickcashteam6/PostJobActivity.java
+++ app/src/main/java/com/team6/quickcashteam6/PostJobActivity.java
@@ -1,6 +1,23 @@
 package com.team6.quickcashteam6;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class PostJobActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PostJobActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postjob);
+
+    }
+
+    @Override
+    public void onClick(View view){
+
+    }
 }
