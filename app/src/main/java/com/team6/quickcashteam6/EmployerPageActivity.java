48fbf05a53e61ca0a790b02d699079f5f0c27fb7
Ayat Kamona <ay245494@dal.ca>
2022-02-05 20:15:31 -0400

    checkIfMovedToPostJobPage() espresso test created

--- app/src/main/java/com/team6/quickcashteam6/EmployerPageActivity.java
+++ app/src/main/java/com/team6/quickcashteam6/EmployerPageActivity.java
@@ -1,4 +1,6 @@
 package com.team6.quickcashteam6;
 
-public class EmployerPageActivity {
+import android.app.Activity;
+
+public class EmployerPageActivity extends Activity {
 }
--- app/src/main/java/com/team6/quickcashteam6/JobData.java
+++ app/src/main/java/com/team6/quickcashteam6/JobData.java
@@ -1,4 +1,6 @@
 package com.team6.quickcashteam6;
 
+import android.app.Activity;
+
 public class JobData {
 }
