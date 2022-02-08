ed6d54c0a0a50e4aeffa5aad4ba2a154fa5b8784
Ayat Kamona <ay245494@dal.ca>
2022-02-07 23:01:47 -0400

    pulling

--- app/src/main/java/com/team6/quickcashteam6/PostJobActivity.java
+++ app/src/main/java/com/team6/quickcashteam6/PostJobActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,4 +29,5 @@ public class PostJobActivity extends AppCompatActivity implements View.OnClickLi
         Intent submitJob = new Intent(PostJobActivity.this, EmployerPageActivity.class);
         startActivity(submitJob);
     }
+
 }
