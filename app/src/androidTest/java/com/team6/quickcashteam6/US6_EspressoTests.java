fda2c7755565b7f7371a892e5b229caf0d4b28fa
Ayat Kamona <ay245494@dal.ca>
2022-02-07 20:40:06 -0400

    checkIfSkillsEmpty() espresso test created

--- app/src/androidTest/java/com/team6/quickcashteam6/US6_EspressoTests.java
+++ app/src/androidTest/java/com/team6/quickcashteam6/US6_EspressoTests.java
@@ -103,4 +103,19 @@ public class US6_EspressoTests {
         onView(withId(R.id.submitJobButton)).perform(click());
         onView(withId(R.id.errorMessage)).check(matches(withText(R.string.EMPTY_START_TIME)));
     }
+
+    /**
+     * Tests if correct error message is shown when skills field is empty.
+     */
+    @Test
+    public void checkIfSkillsEmpty() {
+        onView(withId(R.id.job_title)).perform(typeText("Babysitting"));
+        onView(withId(R.id.job_payment)).perform(typeText("$20/hr"));
+        onView(withId(R.id.start_time)).perform(typeText("2022-02-14 at 8pm"));
+        onView(withId(R.id.skills)).perform(typeText(""));
+        onView(withId(R.id.job_description)).perform(typeText("Babysit a 4 year old"));
+        onView(withId(R.id.submitJobButton)).perform(click());
+        onView(withId(R.id.errorMessage)).check(matches(withText(R.string.EMPTY_SKILLS)));
+    }
+
 }
