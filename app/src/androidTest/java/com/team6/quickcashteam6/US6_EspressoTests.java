a07a96c09f36d1842f71b6363acc2a2072b8687b
Ayat Kamona <ay245494@dal.ca>
2022-02-07 20:35:23 -0400

    checkIfEmptyPayment() espresso test created

--- app/src/androidTest/java/com/team6/quickcashteam6/US6_EspressoTests.java
+++ app/src/androidTest/java/com/team6/quickcashteam6/US6_EspressoTests.java
@@ -76,6 +76,18 @@ public class US6_EspressoTests {
         onView(withId(R.id.errorMessage)).check(matches(withText(R.string.EMPTY_JOB_TITLE)));
     }
 
-
+    /**
+     * Tests if correct error message is shown when payment field is empty.
+     */
+    @Test
+    public void checkIfPaymentEmpty() {
+        onView(withId(R.id.job_title)).perform(typeText("Babysitting"));
+        onView(withId(R.id.job_payment)).perform(typeText(""));
+        onView(withId(R.id.skills)).perform(typeText("Responsible"));
+        onView(withId(R.id.start_time)).perform(typeText("2022-02-14 at 8pm"));
+        onView(withId(R.id.job_description)).perform(typeText("Babysit a 4 year old"));
+        onView(withId(R.id.submitJobButton)).perform(click());
+        onView(withId(R.id.errorMessage)).check(matches(withText(R.string.EMPTY_PAYMENT)));
+    }
 
 }
