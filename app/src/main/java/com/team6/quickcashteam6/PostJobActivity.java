aab85cfaa25c8e30311976f674ec95ae87741d32
Elyas Al Belushi <el992401@dal.ca>
2022-02-07 21:02:14 -0400

    implemented each field as String variables in PostJobActivity

--- app/src/main/java/com/team6/quickcashteam6/PostJobActivity.java
+++ app/src/main/java/com/team6/quickcashteam6/PostJobActivity.java
@@ -5,10 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PostJobActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText insert_job_title = findViewById(R.id.insert_job_title);
+    private EditText insert_skills = findViewById(R.id.insert_skills);
+    private EditText insert_job_description = findViewById(R.id.insert_job_description);
+    private EditText insert_job_payment = findViewById(R.id.insert_job_payment);
+    private EditText insert_start_time = findViewById(R.id.insert_start_time);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +25,25 @@ public class PostJobActivity extends AppCompatActivity implements View.OnClickLi
 
         Button submitJobButton = findViewById(R.id.submitJobButton);
         submitJobButton.setOnClickListener(this);
+
+    }
+
+    protected void setErrorMessage(String message){
+        TextView errorMessage = findViewById(R.id.errorMessage);
+        errorMessage.setText(message.trim());
     }
 
     @Override
     public void onClick(View view){
+
+        String job_title = insert_job_title.getText().toString().trim();
+        String skills = insert_skills.getText().toString().trim();
+        String job_description = insert_job_description.getText().toString().trim();
+        String job_payment = insert_job_payment.getText().toString().trim();
+        String start_time = insert_start_time.getText().toString().trim();
+
+
+
         openEmployerPage();
     }
 
@@ -28,4 +51,5 @@ public class PostJobActivity extends AppCompatActivity implements View.OnClickLi
         Intent submitJob = new Intent(PostJobActivity.this, EmployerPageActivity.class);
         startActivity(submitJob);
     }
+
 }
