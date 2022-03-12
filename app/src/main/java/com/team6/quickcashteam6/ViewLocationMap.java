7e6cd1790e03b89aa6bcb5abf5710fa6e0444728
Ayat Kamona <ay245494@dal.ca>
2022-03-11 22:17:17 -0400

    implemented show location button for employees with map integration

---
+++ app/src/main/java/com/team6/quickcashteam6/ViewLocationMap.java
@@ -0,0 +1,60 @@
+package com.team6.quickcashteam6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.team6.quickcashteam6.databinding.ActivityViewLocationMapBinding;
+
+public class ViewLocationMap extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityViewLocationMapBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityViewLocationMapBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        double lng;
+        double lat;
+        Bundle extras = getIntent().getExtras();
+        if(extras!=null) {
+            lng = Double.parseDouble(extras.getString("lng"));
+            lat = Double.parseDouble(extras.getString("lat"));
+            LatLng sydney = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Job Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+
+    }
+}
--- app/src/main/java/com/team6/quickcashteam6/employeeJobAdapter.java
+++ app/src/main/java/com/team6/quickcashteam6/employeeJobAdapter.java
@@ -1,43 +1,56 @@
 package com.team6.quickcashteam6;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 // Reference Used: https://www.geeksforgeeks.org/how-to-populate-recyclerview-with-firebase-data-using-firebaseui-in-android-studio/
 
-public class employeeJobAdapter extends FirebaseRecyclerAdapter<
-        JobData, employeeJobAdapter.jobsViewholder> {
+public class employeeJobAdapter extends FirebaseRecyclerAdapter<JobData, employeeJobAdapter.jobsViewholder> {
 
     public employeeJobAdapter(@NonNull FirebaseRecyclerOptions<JobData> options) {
         super(options);
     }
 
     @Override
-    protected void
-    onBindViewHolder(@NonNull jobsViewholder holder, int position, @NonNull JobData model) {
+    protected void onBindViewHolder(@NonNull jobsViewholder holder, int position, @NonNull JobData model) {
         holder.job_titles.setText(model.getJobTitle());
         holder.job_payments.setText(model.getPayment());
         holder.job_skills.setText(model.getSkills());
         holder.start_times.setText(model.getStartTime());
         holder.job_descriptions.setText(model.getJobDescription());
+        holder.lng.setText(Double.toString(model.getLng()));
+        holder.lat.setText(Double.toString(model.getLat()));
+
     }
 
     @NonNull
     @Override
-    public jobsViewholder
-    onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public jobsViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.job_post_card, parent, false);
         return new employeeJobAdapter.jobsViewholder(view);
     }
 
     class jobsViewholder extends RecyclerView.ViewHolder {
-        TextView job_titles, job_payments, job_skills, start_times, job_descriptions;
+        TextView job_titles;
+        TextView job_payments;
+        TextView job_skills;
+        TextView start_times;
+        TextView job_descriptions;
+        TextView lng;
+        TextView lat;
+        Button job_location;
+
         public jobsViewholder(@NonNull View itemView) {
             super(itemView);
 
@@ -46,6 +59,20 @@ public class employeeJobAdapter extends FirebaseRecyclerAdapter<
             job_skills = itemView.findViewById(R.id.job_skills);
             start_times = itemView.findViewById(R.id.start_times);
             job_descriptions = itemView.findViewById(R.id.job_descriptions);
+            lng = itemView.findViewById(R.id.lng);
+            lat = itemView.findViewById(R.id.lat);
+            job_location = itemView.findViewById(R.id.job_location);
+            job_location.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(view.getContext(), ViewLocationMap.class);
+                    String mapLng = lng.getText().toString();
+                    String mapLat = lat.getText().toString();
+                    intent.putExtra("lng", mapLng);
+                    intent.putExtra("lat", mapLat);
+                    view.getContext().startActivity(intent);
+                }
+            });
         }
     }
 }
