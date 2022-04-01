9ef34de5fb204cd196cfdc8390afbd2a2ec44361
Your Name <you@example.com>
2022-04-01 11:36:44 -0300

    Employee ID added and fixed

--- app/src/main/java/com/team6/quickcashteam6/MainActivity.java
+++ app/src/main/java/com/team6/quickcashteam6/MainActivity.java
@@ -8,6 +8,10 @@ public class MainActivity extends AppCompatActivity {
 
     public static double jobLongtitute;
     public static double jobLatitude;
+    public static String employeeID;
+    public static String employeeKey;
+    public static String employeeName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
