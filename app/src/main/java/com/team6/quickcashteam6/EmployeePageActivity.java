56b7f9c36e61e2caf124f8753a0e03f5c16cdfbd
Ayat Kamona <ay245494@dal.ca>
2022-04-03 21:40:27 -0300

    pulling

--- app/src/main/java/com/team6/quickcashteam6/EmployeePageActivity.java
+++ app/src/main/java/com/team6/quickcashteam6/EmployeePageActivity.java
@@ -16,6 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
